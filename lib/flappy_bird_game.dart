e1a55be705da84e18900a8f8b0b502ad925a597f
MuhamedUssama <mohamed.osama200538@gmail.com>
2025-11-19 22:35:31 +0200

    Created background

--- lib/flappy_bird_game.dart
+++ lib/flappy_bird_game.dart
@@ -2,22 +2,29 @@ import 'dart:async';
 
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flappy_bird/components/background.dart';
 import 'package:flappy_bird/components/bird.dart';
 
 class FlappyBirdGame extends FlameGame with TapCallbacks {
   /*
     Basic Game Components
-    - Bird
     - Background
+    - Bird
     - Ground
     - Pipes
     - Score
   */
 
   late Bird bird;
+  late Background background;
 
   @override
   FutureOr<void> onLoad() {
+    // Load Background
+    background = Background(size);
+    add(background);
+
+    // Load Bird
     bird = Bird();
     add(bird);
   }
